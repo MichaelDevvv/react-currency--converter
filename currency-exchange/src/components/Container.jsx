b33213be87b62c9598f694c7bb73d95e1c441601
Michal <78083264+MichaelDevvv@users.noreply.github.com>
2022-12-08 20:35:38 +0100

    update

--- currency-exchange/src/components/Container.jsx
+++ currency-exchange/src/components/Container.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion, AnimatePresence, MotionConfig } from 'framer-motion';
 import axios from 'axios';
 
 import '../index.css';
@@ -39,7 +39,7 @@ const Container = () => {
   return (
     <div className="w-screen h-screen lg:w-[75%] lg:h-[500px] bg-transparent flex flex-col lg:flex-row items-center justify-around rounded-lg">
       <div className="bg-white/[0.19] rounded-2xl backdrop-blur-[7.1px] shadow-[0_4px_30px_rgba(0,0,0,0.1)] h-full w-full flex flex-col items-center justify-center gap-4">
-        <div className="lg:w-[60%] w-[70%] flex h-full flex-col items-center justify-center gap-4">
+        <div className="lg:w-[60%] w-[75%] flex h-full flex-col items-center justify-center gap-4">
           <label htmlFor="name" className="w-full relative">
             <input
               type="number"
@@ -53,24 +53,26 @@ const Container = () => {
             </span>
           </label>
 
-          <div className="w-full flex flex-col gap-2 relative">
-            {selectedCurrencies.first && (
-              <motion.div
-                initial={{ x: -180, y: 100, rotate: -60, scale: 0, opacity: 0 }}
-                animate={{ x: 0, y: 0, rotate: 0, scale: 1, opacity: 1 }}
-                exit={{ x: -180, y: 100, rotate: -60, opacity: 0 }}
-                className="absolute right-2 top-2 z-20 cursor-pointer text-white"
-              >
-                <RxCross2
-                  size={30}
-                  onClick={() => {
-                    setSelectedCurrencies({ ...selectedCurrencies, first: null });
-                    setSearchedTerm('');
-                    setSearchedCurrencies(currencies);
-                  }}
-                />
-              </motion.div>
-            )}
+          <div className="w-full flex flex-col gap-2 relative overflow-hidden">
+            <AnimatePresence>
+              {selectedCurrencies.first && (
+                <motion.div
+                  initial={{ x: -180, y: 100, rotate: -60, scale: 0, opacity: 0 }}
+                  animate={{ x: 0, y: 0, rotate: 0, scale: 1, opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  className="absolute right-2 top-2 z-20 cursor-pointer text-white"
+                >
+                  <RxCross2
+                    size={30}
+                    onClick={() => {
+                      setSelectedCurrencies({ ...selectedCurrencies, first: null });
+                      setSearchedTerm('');
+                      setSearchedCurrencies(currencies);
+                    }}
+                  />
+                </motion.div>
+              )}
+            </AnimatePresence>
             <input
               type="text"
               className="p-2 w-full outline-none base placeholder:opacity-0 border-[1px] rounded-lg bg-transparent text-lg text-white"
@@ -89,25 +91,29 @@ const Container = () => {
             />
             <AnimatePresence>
               {!selectedCurrencies.first && (
-                <motion.div
-                  initial={{ x: -180, y: 100, rotate: -60, scale: 0, opacity: 0 }}
-                  animate={{ x: 0, y: 0, rotate: 0, scale: 1, opacity: 1 }}
-                  exit={{ x: -180, y: 100, rotate: -60, opacity: 0 }}
-                  className="h-[200px] text-white overflow-y-auto flex flex-col border-[1px] p-2 rounded-lg"
-                >
-                  {searchedCurrencies.map((curr) => (
-                    <div
-                      key={curr.code}
-                      className="flex justify-between cursor-pointer"
-                      onClick={() =>
-                        setSelectedCurrencies({ ...selectedCurrencies, first: curr.code })
-                      }
-                    >
-                      <span>{curr.code}</span>
-                      <span>{curr.name}</span>
-                    </div>
-                  ))}
-                </motion.div>
+                <MotionConfig transition={{ duration: 0.2 }}>
+                  <motion.div
+                    initial={{ x: -180, y: 100, rotate: -60, scale: 0, opacity: 0 }}
+                    animate={{ x: 0, y: 0, rotate: 0, scale: 1, opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    className="h-[200px] text-white overflow-y-auto flex flex-col border-[1px] rounded-lg"
+                  >
+                    {searchedCurrencies.map((curr) => (
+                      <div
+                        key={curr.code}
+                        className="flex justify-between cursor-pointer p-2 hover:bg-[#4568dc]"
+                        onClick={() =>
+                          setSelectedCurrencies({ ...selectedCurrencies, first: curr.code })
+                        }
+                      >
+                        <span>{curr.code}</span>
+                        <span>
+                          {curr.name.length < 20 ? curr.name : `${curr.name.slice(0, 10)}...`}
+                        </span>
+                      </div>
+                    ))}
+                  </motion.div>
+                </MotionConfig>
               )}
             </AnimatePresence>
           </div>
